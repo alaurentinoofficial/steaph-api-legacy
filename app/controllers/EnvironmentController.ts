b10c5fb5e7071e2607f527f04e72ec7870ddd2e6
Anderson Laurentino <alaurentino.br@gmail.com>
2017-07-10 21:58:29 -0300

    Add controllers

---
+++ app/controllers/EnvironmentController.ts
@@ -0,0 +1,92 @@
+import * as jwt from "jsonwebtoken";
+
+import { Request, Response } from 'express';
+import { Environment, EnvironmentSchema } from "../models/Environment";
+import { SolutionSchema, Solution } from "../models/Solution";
+import { Server } from "../../server";
+
+export class EnvironmentController {
+    public static Get (req: Request, res:  Response) {
+        let token = req.headers["authorization"].replace("JWT ", "");
+        let payload = jwt.verify(token ? token : "", Server.get('crypt_key'))._doc;
+
+        SolutionSchema.findOne({user: payload})
+        .then(s => {
+            EnvironmentSchema.find({solution: s})
+            .then(env => {
+                res.status(200).json(env);
+            })
+            .catch(err => {
+                res.status(500).json([]);
+            });
+        })
+        .catch(err => {
+            res.status(500).json([]);
+        });
+    }
+
+    public static Post (req: Request, res:  Response) {
+        let token = req.headers['authorization'].replace("JWT ", "");
+        let payload = jwt.verify(token ? token : "", Server.get('crypt_key'))._doc;
+
+        SolutionSchema.find({user: payload}, (err, solution: Solution) => {
+            if(!solution || err)
+                return res.json({status: false, message: "Invalid solution!"});
+
+            var env = {solution: payload.solution, name: req.body.name}
+
+            EnvironmentSchema.create(env, (err, docs) => {
+                if(err) {
+                    if(err.code == 11000)
+                        res.json({status: false, message: "Environment already created!", code: err});
+                    else
+                        res.json({status: false, message: "Invalid environment!", code: err.code});
+                    return;
+                }
+
+                res.json({status: true, message: "Succefuly created environment!"});
+            });
+        });
+    };
+
+    public static PutById (req: Request, res:  Response) {
+        let env: Environment = req.body;
+        if(!req.params.id || !env)
+            return res.json({status: false, message: "Invalid environment!"});
+        
+        let token = req.headers['authorization'].replace("JWT ", "");
+        let payload = jwt.verify(token ? token : "", Server.get('crypt_key'))._doc;
+
+        SolutionSchema.find({user: payload}, (err, solution: Solution) => {
+            if(!solution || err)
+                return res.json({status: false, message: "Invalid solution!"});
+
+            EnvironmentSchema.findOneAndUpdate({_id: req.params.id}, env, {upsert: true}, (err) => {
+                if(err)
+                    return res.json({status: false, message: "Invalid environment!"});
+                
+                res.json({status: true, message: "Succefuly updated environment!"});
+            });
+        });
+    };
+
+    public static DeleteById (req: Request, res:  Response) {
+        if(!req.params.id)
+            return res.json({status: false, message: "Invalid environment!"});
+
+        let token = req.headers["authorization"].replace("JWT ", "");
+        let payload = jwt.verify(token ? token : "", Server.get('crypt_key'))._doc;
+
+        SolutionSchema.findOne({user: payload}, (err, docs: Solution) => {
+            if(err || !docs)
+                return res.json({status: false, message: "Invalid token!"});
+
+            EnvironmentSchema.remove({solution: docs, _id: req.params.id}, (err, d) => {
+                if(err)
+                    return res.json({status: false, message: "Invalid environment!"});
+                
+                res.json({status: true, message: "Environment deleted!"});
+            })
+        });
+    }
+}
---
+++ app/controllers/UserController.ts
@@ -0,0 +1,62 @@
+import * as jwt from "jsonwebtoken";
+
+import { UserSchema } from "../models/User";
+import { Server } from "../../server";
+import { SolutionSchema, Solution } from "../models/Solution";
+
+export class UserController {
+    public static Login (req, res) {
+        UserSchema.findOne({email: req.body.email}, function(err, user) {
+            if(err) {
+                console.log("Error at Login on UserController");
+                res.json({success: false, message: 'Invalid email'});
+                return;
+            }
+
+            if(!user)
+                res.json({success: false, message: 'Invalid email'});
+            else {
+                user.comparePassword(req.body.password, function(err, isMatch) {
+                    if(isMatch && !err)
+                    {
+                        let sName: string;
+                        SolutionSchema.findOne({_id: user.solution}, (err, solution: Solution) => {
+                            if(err || !solution) return;
+                            sName = solution.name;
+                        });
+
+                        var expiresTime = req.body.selected === true ? 160800 : 60000;
+
+                        var token = jwt.sign(user, Server.get('crypt_key'), { expiresIn: expiresTime });
+                        var json = {solution: user.solution, solutionName: user.fullName, email: user.email, token: 'JWT ' + token};
+
+                        res.json(json);
+                    }
+                    else
+                        res.json({success: false, message: 'Invalid password'});
+                });
+            }
+        });
+    }
+
+    public static Register (req, res) {
+        SolutionSchema.find({_id: req.body.solution}, (err, solution) => {
+            if(solution.length == 0 || err) {
+                res.json({status: false, message: "Invalid solution!"});
+                return;
+            }
+
+            UserSchema.create(req.body, function(err, docs) {
+                if(err) {
+                    if(err.code == 11000)
+                        res.json({success: false, message: 'That email address already exits!'});
+                    else
+                        res.json({success: false, message: 'Invalid email or password!'});
+                    return;
+                }
+
+                res.json({success: true, message: 'Successfuly created new user'});
+            });
+        });
+    }
+}
